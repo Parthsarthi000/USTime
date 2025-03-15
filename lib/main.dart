a9eb441bf7663be6a1ac9f375793d0c8c4480ee8
Parthsarthi Kalra <parthsarthikalra12@gmail.com>
2025-03-16 02:20:38 +0800

    Implemented the logic for checking if user opened the app first time or not.

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,51 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  late bool isFirstTime;
+  @override
+  void initState() {
+    super.initState();
+    isUsersFirstTime();
+  }
+
+  Future<void> isUsersFirstTime() async {
+    final prefs = await SharedPreferences.getInstance();
+    if ((prefs.getBool("isFirstTime") ?? true) == true) {
+      //if its users first time
+      await prefs.setBool("isFirstTime",
+          false); //set it to false to prevent future opens accesing the login screen
+      isFirstTime = true;
+    } else {
+      isFirstTime = false; //not the first time
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Broductivity',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 36, 101, 127)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: isFirstTime
+          ? const FirstTimePage(title: '')
+          : const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+//ToDo
+//Create respective state widgets for both
